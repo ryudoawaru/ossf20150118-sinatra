65317d51cb647001cbd8f1e0b216a9c693378160
ryudoawaru <ryudoawaru@gmail.com>
2015-01-18 06:53:40 +0000

    Add show action

--- img_list.rb
+++ img_list.rb
@@ -1,15 +1,32 @@
 class ImgList < Sinatra::Base
 
   Images = [
-    {url: 'http://i.imgur.com/LIPAV4D.jpg', title: 'The incredible paintings of Rob Gonsalves 1'},
-    {url: 'http://i.imgur.com/y40a93x.jpg', title: 'Image2'},
     {
+      id: 1,
+      url: 'http://i.imgur.com/LIPAV4D.jpg', title: 'The incredible paintings of Rob Gonsalves 1'},
+    {
+     id: 2,
+     url: 'http://i.imgur.com/y40a93x.jpg', title: 'Image2'},
+    {
+       id: 3,
       url: 'http://i.imgur.com/hswP6Mz.png', title: 'A moment of silence'
     }
   ]
 
   get '/imgs' do
     erb :'index.html'
+  end
+
+  get '/imgs/:id' do
+    Images.each do |img|
+      p img
+      p params[:id]
+      if img[:id] == params[:id].to_i
+        @img = img
+        break
+      end
+    end
+    erb :'show.html'
   end
 
 
